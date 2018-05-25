61f9ba9ec364b18a5db22a5f8b16b81be876711b
Shivam Patel <spatel4140@gmail.com>
2018-05-25 15:20:09 -0400

    added trending search term capability

--- BingRewards/BingRewards.py
+++ BingRewards/BingRewards.py
@@ -1,8 +1,7 @@
 import sys
 import os
-from src.driver import Rewards, Completion
-from datetime import datetime, timedelta
-from dateutil import tz
+from src.rewards import Rewards
+from src.log import HistLog
 import logging
 
 
@@ -11,100 +10,17 @@ DRIVER                     = "chromedriver"
 
 LOG_DIR                    = "logs"
 ERROR_LOG                  = "error.log"
-HIST_LOG                   = "hist.log"
+RUN_LOG                    = "run.log"
+SEARCH_LOG                 = "search.log"
 
 DEBUG                      = True
 HEADLESS                   = True
 
 
+## TODO
+#
+#  
 
-class HistLog:
-    __DATETIME_FORMAT      = "%a, %b %d %Y %I:%M%p %Z"
-
-    __LOCAL_TIMEZONE       = tz.tzlocal()
-    __PST_TIMEZONE         = tz.gettz("US/Alaska") # Alaska timezone, guards against Pacific Daylight Savings Time
-
-    __RESET_HOUR           = 0  # AM PST
-    __MAX_HIST_LEN         = 30 # days
-
-    __COMPLETED_TRUE       = "Successful"
-    __COMPLETED_FALSE      = "Failed {}"
-
-    __WEB_SEARCH_OPTION    = "Web Search"
-    __MOBILE_SEARCH_OPTION = "Mobile Search"
-    __OFFERS_OPTION        = "Offers"
-
-
-    def __init__(self, path, run_datetime=datetime.now()):
-        self.path = path
-        self.run_datetime = run_datetime.replace(tzinfo=self.__LOCAL_TIMEZONE)
-        self.hist = self.__read()
-        self.__completion = Completion()
-
-    def __read(self):
-        if not os.path.exists(self.path):
-            return []
-        else:
-            with open(self.path, "r") as log:
-                return [line.strip("\n") for line in log.readlines()]
-
-    def get_timestamp(self):
-        return self.run_datetime.strftime(self.__DATETIME_FORMAT)
-    def get_completion(self):
-        # check if already ran today
-        if len(self.hist) > 0:
-            last_ran, completed = self.hist[-1].split(": ")
-
-            last_ran_pst = datetime.strptime(last_ran, self.__DATETIME_FORMAT).replace(tzinfo=self.__LOCAL_TIMEZONE).astimezone(self.__PST_TIMEZONE)
-            run_datetime_pst = self.run_datetime.astimezone(self.__PST_TIMEZONE)
-            delta_days = (run_datetime_pst.date()-last_ran_pst.date()).days
-            if ((delta_days == 0 and last_ran_pst.hour >= self.__RESET_HOUR) or 
-                (delta_days == 1 and run_datetime_pst.hour < self.__RESET_HOUR)):
-                
-                if completed == self.__COMPLETED_TRUE:
-                    self.__completion.web_search = True
-                    self.__completion.mobile_search = True
-                    self.__completion.offers = True
-                else:
-                    #self.hist.pop()
-                    if self.__WEB_SEARCH_OPTION not in completed:
-                        self.__completion.web_search = True
-                    if self.__MOBILE_SEARCH_OPTION not in completed:
-                        self.__completion.mobile_search = True
-                    if self.__OFFERS_OPTION not in completed:
-                        self.__completion.offers = True
-
-        if not self.__completion.is_all_completed():
-            # update hist with todays time stamp
-            self.hist.append(self.get_timestamp())
-            if len(self.hist) == self.__MAX_HIST_LEN:
-                self.hist = self.hist[1:]
-
-        return self.__completion
-    def write(self, completion):
-        self.__completion.update(completion)
-        if not self.__completion.is_all_completed():
-            if not self.__completion.is_any_completed():
-                failed = "{}, {} & {}".format(self.__WEB_SEARCH_OPTION, self.__MOBILE_SEARCH_OPTION, self.__OFFERS_OPTION)
-            elif not self.__completion.is_any_searches_completed():
-                failed = "{} & {}".format(self.__WEB_SEARCH_OPTION, self.__MOBILE_SEARCH_OPTION)
-            elif not self.__completion.is_web_search_completed() and not self.__completion.is_offers_completed():
-                failed = "{} & {}".format(self.__WEB_SEARCH_OPTION, self.__OFFERS_OPTION)
-            elif not self.__completion.is_mobile_search_completed() and not self.__completion.is_offers_completed():
-                failed = "{} & {}".format(self.__MOBILE_SEARCH_OPTION, self.__OFFERS_OPTION)
-            elif not self.__completion.is_offers_completed():
-                failed = "{}".format(self.__OFFERS_OPTION)
-            elif not self.__completion.is_mobile_search_completed():
-                failed = "{}".format(self.__MOBILE_SEARCH_OPTION)
-            else:
-                failed = "{}".format(self.__WEB_SEARCH_OPTION)
-            msg = self.__COMPLETED_FALSE.format(failed) 
-        else:
-            msg = self.__COMPLETED_TRUE
-
-        self.hist[-1] = "{}: {}".format(self.hist[-1], msg)
-        with open(self.path, "w") as log:
-            log.write("\n".join(self.hist) + "\n")
 
 
 def __main(arg0, arg1):
@@ -117,7 +33,7 @@ def __main(arg0, arg1):
 
     if not os.path.exists(LOG_DIR):
         os.makedirs(LOG_DIR)
-    hist_log = HistLog(os.path.join(LOG_DIR, HIST_LOG))
+    hist_log = HistLog(os.path.join(LOG_DIR, RUN_LOG), os.path.join(LOG_DIR, SEARCH_LOG))
 
     # get credentials
     try:
@@ -136,50 +52,50 @@ def __main(arg0, arg1):
 
     if arg1 in ["w", "web"]:
         print("\n\t{}\n".format("You selected web search"))
-        rewards.complete_web_search()
+        rewards.complete_web_search(hist_log.get_search_hist())
         if not hist_log.get_completion().is_web_search_completed():
-            hist_log.write(rewards.completion)
+            hist_log.write(rewards.completion, rewards.search_hist)
     elif arg1 in ["m", "mobile"]:
         print("\n\t{}\n".format("You selected mobile search"))
-        rewards.complete_mobile_search()
+        rewards.complete_mobile_search(hist_log.get_search_hist())
         if not hist_log.get_completion().is_mobile_search_completed():
-            hist_log.write(rewards.completion)
+            hist_log.write(rewards.completion, rewards.search_hist)
     elif arg1 in ["b", "both"]:
         print("\n\t{}\n".format("You selected both searches"))
-        rewards.complete_both_searches()
+        rewards.complete_both_searches(hist_log.get_search_hist())
         if not hist_log.get_completion().is_both_searches_completed():
-            hist_log.write(rewards.completion)
+            hist_log.write(rewards.completion, rewards.search_hist)
     elif arg1 in ["o", "other"]:
         print("\n\t{}\n".format("You selected offers"))
         rewards.complete_offers()
         if not hist_log.get_completion().is_offers_completed():
-            hist_log.write(rewards.completion)
+            hist_log.write(rewards.completion, rewards.search_hist)
     elif arg1 in ["a", "all"]:
         print("\n\t{}\n".format("You selected all"))
-        rewards.complete_all()
+        rewards.complete_all(hist_log.get_search_hist())
         if not hist_log.get_completion().is_all_completed():
-            hist_log.write(rewards.completion)
+            hist_log.write(rewards.completion, rewards.search_hist)
     else:
         print("\n\t{}\n".format("You selected remianing"))
         try:
             completion = hist_log.get_completion()
             if not completion.is_all_completed():
                 if not completion.is_any_completed():
-                    rewards.complete_all()
+                    rewards.complete_all(hist_log.get_search_hist())
                 elif not completion.is_any_searches_completed():
-                    rewards.complete_both_searches()
+                    rewards.complete_both_searches(hist_log.get_search_hist())
                 elif not completion.is_web_search_completed() and not completion.is_offers_completed():
-                    rewards.complete_web_search_and_offers()
+                    rewards.complete_web_search_and_offers(hist_log.get_search_hist())
                 elif not completion.is_mobile_search_completed() and not completion.is_offers_completed():
-                    rewards.complete_mobile_search(print_stats=False)
+                    rewards.complete_mobile_search(hist_log.get_search_hist(), print_stats=False)
                     rewards.complete_offers()
                 elif not completion.is_offers_completed():
                     rewards.complete_offers()
                 elif not completion.is_mobile_search_completed():
-                    rewards.complete_mobile_search()
+                    rewards.complete_mobile_search(hist_log.get_search_hist())
                 else:
-                    rewards.complete_web_search()
-                hist_log.write(rewards.completion)
+                    rewards.complete_web_search(hist_log.get_search_hist())
+                hist_log.write(rewards.completion, rewards.search_hist)
 
                 completion = hist_log.get_completion()
                 if not completion.is_all_completed(): # check again, log if any failed
@@ -196,20 +112,15 @@ def __main(arg0, arg1):
             logging.exception(hist_log.get_timestamp())
             logging.debug("")
 
-            hist_log.write(rewards.completion)
+            hist_log.write(rewards.completion, rewards.search_hist)
             raise
     
-
 
 if __name__ == "__main__":
     args = sys.argv
     if len(args) == 1:
-        input_message = "Enter \t{}, \n\t{}, \n\t{}, \n\t{}, \n\t{}, \n\t{} \nInput: \t".format("w for web", 
-                                                                                                "m for mobile", 
-                                                                                                "b for both", 
-                                                                                                "o for offers", 
-                                                                                                "a for all",
-                                                                                                "r for remaining (default)")
+        out = "Enter \t{}, \n\t{}, \n\t{}, \n\t{}, \n\t{}, \n\t{} \nInput: \t"
+        input_message = out.format("w for web", "m for mobile", "b for both", "o for offers", "a for all","r for remaining (default)")
         try:
             arg1 = raw_input(input_message) # python 2
         except:
